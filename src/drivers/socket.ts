7d47bfdd6748959361b87f7321ae3956885cedcd
Graham Neilson <graham.c.neilson@gmail.com>
2018-08-11 20:12:17 +0100

    split socket lib

--- src/drivers/socket.ts
+++ src/drivers/socket.ts
@@ -1,4 +1,3 @@
-import * as io from 'socket.io-client/dist/socket.io.slim'
 import {adapt} from '@cycle/run/lib/adapt'
 import { Stream } from 'xstream'
 
@@ -6,21 +5,14 @@ export function makeSocketDriver(socketUrl) {
 	const socketDriver = (/* no sinks */) => {
 		const incoming$: Stream<any> = Stream.create({
 			start: listener => {
-				const socket = io(socketUrl)
-
-				socket.on('connect', function() {
-					console.log('Connected')
-				})
-
-				socket.on('pushUpdate', function(event) {
-					listener.next(event)
-				})
-
-				socket.emit('liveSportsConfig', {
-					event: 2000,
-					market: 1000,
-					outcome: 1000,
-				})
+				const interval = setInterval(function connect(index) {
+					try {
+						const socket = io(socketUrl)
+						clearInterval(interval)
+						initialiseSocket(socket, listener)
+					} catch {
+					}
+				}, 500)
 			},
 			stop: () => {},
 		})
@@ -31,3 +23,18 @@ export function makeSocketDriver(socketUrl) {
 	return socketDriver
 }
 
+function initialiseSocket(socket, listener) {
+	socket.on('connect', function() {
+		console.log('Connected')
+	})
+
+	socket.on('pushUpdate', function(event) {
+		listener.next(event)
+	})
+
+	socket.emit('liveSportsConfig', {
+		event: 2000,
+		market: 1000,
+		outcome: 1000,
+	})
+}
